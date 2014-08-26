a3f419469ce517ae96ef0534ab90129c82f0b717
David Heijkamp <david.heijkamp@naturalis.nl>
2014-08-26 22:47:11 +0200

    Really added all the changes from the original repo

--- templates/head.php
+++ templates/head.php
@@ -11,4 +11,5 @@
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
